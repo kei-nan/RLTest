b80d115feb5ca324697cc3c9e820b3c7fc4c7d2a
jonathan keinan <jonathan.keinan@redis.com>
2025-12-08 13:46:40 +0200

    update packages

--- RLTest/_version.py
+++ RLTest/_version.py
@@ -1,8 +1,9 @@
 
 # This attribute is the only one place that the version number is written down,
 # so there is only one place to change it when the version number changes.
+from importlib.metadata import version
+
 try:
-    import pkg_resources
-    __version__ = pkg_resources.get_distribution('RLTest').version
-except (pkg_resources.DistributionNotFound, AttributeError, ImportError):
+    __version__ = version('RLTest')
+except Exception:
     __version__ = "99.99.99"  # like redis modules
